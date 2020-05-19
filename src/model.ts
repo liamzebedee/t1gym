c25fdc88602f90d0983bf0f150b262dafb2eca9b
liamzebedee <liamzebedee@yahoo.com.au>
2020-05-19 16:12:28 +1000

    Replace insulin/food/exercise models with functional compositioning.

--- src/model.ts
+++ src/model.ts
@@ -31,7 +31,7 @@ let metabolism
 
 class BodyMetabolismModel {
     // Insulin sensitivity is the ratio of insulin units to blood glucose reduction.
-    getInsulinSensitivity(t): number {
+    getInsulinSensitivity(): number {
         return -1.8
     }
 
@@ -65,98 +65,11 @@ export let fiaspInsulinModel = `
 `.split('\n').filter(x => !!x).map(line => line.match(/\S+/g).map(x => parseFloat(x)))
 // console.log('fiasp', fiaspInsulinModel)
 
-
-function fiaspInsulinEffect({ 
-    metabolism,
-    amount,
-    injectionDate,
-    insulinModel = fiaspInsulinModel
-}) {
-    let active = true
-    
-    // Returns insulin applied between t and t+DURATION
-    return ({ date, sgv, duration }) => {
-        if(!active) return 0
-
-        let x1 = (date - injectionDate)
-        let x2 = x1 + duration
-        
-        // map time to hours
-        x1 = x1 / HOUR
-        x2 = x2 / HOUR
-        if(x1 < 0) return 0
-        
-        const netInsulin = linear(
-            [x1, x2],
-            fiaspInsulinModel.map(a => a[0]),
-            fiaspInsulinModel.map(a => a[1])
-        )
-
-        if(netInsulin[0] >= 100) {
-            active = false
-        }
-
-        const activeInsulinFraction = (netInsulin[1] - netInsulin[0]) / 100
-        
-        // Calculate amount.
-        const activeInsulinAmount = activeInsulinFraction * amount
-        // console.log(`active insulin ${activeInsulinFraction}, ${activeInsulinAmount} U`)
-
-        return activeInsulinAmount * metabolism.getInsulinSensitivity()
-    }
-}
-
 type MealType = 'protein' | 'carbs'
 interface FoodEffect {
     mealType: MealType
     amount: number
     glycemicIndex: number
-}
-
-function foodEffect({
-    mealType,
-    amount,
-    glycemicIndex,
-    ingestionDate
-}) {
-    let active = true
-    
-    // Protein rule is.
-    // Divide whatever the amount is by 5.
-    // Idk why.
-    if(mealType == 'protein') {
-        amount /= 5
-    }
-
-    glycemicIndex = glycemicIndex / 100
-
-    return ({ date, sgv, duration }) => {
-        if(!active) return 0
-        // Food is a linear release.
-        // Weighted by the glycemic index.
-
-        // Individual responses vary a bit.
-        // http://www.cell.com/cell/fulltext/S0092-8674(15)01481-6
-        let x1 = (date - ingestionDate)
-        let x2 = x1 + duration
-
-        // A lower glycemic load means it releases over a longer duration.
-        // My longest duration is set at 6hrs as an example.
-        // https://care.diabetesjournals.org/content/31/12/2281
-        const MAX_ABSORPTION_TIME = (6*HOUR) * (1-glycemicIndex)
-        let digested = x => {
-            if(x > MAX_ABSORPTION_TIME) return 0
-            const digestedRatio = 1 - (x / MAX_ABSORPTION_TIME)
-            return digestedRatio * amount
-        }
-        
-        const a1 = digested(x1)
-        const a2 = digested(x2)
-        let netCarbs = Math.abs(a1 - a2)
-        // console.log(a2, a1, `carbs: ${netCarbs.toPrecision(2)}g`)
-        
-        return netCarbs * metabolism.getCarbSensitivty()
-    }
 }
 
 const ExerciseIntensity = {
@@ -187,7 +100,15 @@ export const functions = {
         }
     },
 
-    food(type, amount, glycemicIndex) {
+    insulinGlucoseEffect(insulinActive) {
+        // const insulinActive = functions.fiaspInsulinActive(amount)
+        return (u, b, c) => {
+            console.log(u, b, c, insulinActive(u), metabolism.getInsulinSensitivity())
+            return insulinActive(u) * metabolism.getInsulinSensitivity()
+        }
+    },
+
+    foodDigestion(type, amount, glycemicIndex) {
         let amount2 = amount
         if(type == 'protein') {
             amount2 /= 5
@@ -206,39 +127,41 @@ export const functions = {
             const duration = (1-glycemicIndex) * 6*HOUR
             return amount * Math.min(u, duration) / duration
         }
-    }
-}
+    },
 
-// Exercise is modelled as a linear curve.
-export function exerciseEffect({ start, duration, intensity }) {
-    // `u` denotes relative time
-    const F = (u) => {
-        return (intensity * BURN_RATE) * (u/duration)
-    }
-
-    return window({
-        start,
-        end: start + duration,
-        F
-    })
-}
-
-function window({ start, end, F }) {
-    return (t) => {
-        if(t < start) return 0
-        if(t > start && t < end) {
-            const u = t - start
-            return F(u)
+    foodDigestionEffect(foodDigestion) {
+        return (u) => {
+            // console.log(u, b, c, insulinActive(u), metabolism.getInsulinSensitivity())
+            return foodDigestion(u) * metabolism.getCarbSensitivty()
         }
-        if(t >= end) return F(end - start)
+    },
+
+    // A window of time.
+    // 0 until `start`, then returns elapsed mS up until a max of `duration`
+    window({ start, duration }) {
+        const elapsed = t => t - start
+        return (t) => {
+            if(t < start) return 0
+            return Math.min(elapsed(t), duration) * 1.
+        }
+    },
+
+    beginsAfter({ start }) {
+        const elapsed = t => t - start
+        return (t) => {
+            if(t < start) return 0
+            return elapsed(t)
+        }
     }
 }
 
-function insulinEffect() {
-    return ({ date, sgv, duration }) => {
-        return 0
-    }
-}
+
+// Functional enhancer.
+export const compose = (...fns) =>
+    fns.reduceRight((prevFn, nextFn) =>
+        (...args) => nextFn(prevFn(...args)),
+        value => value
+    )
 
 
 class Model {
@@ -262,31 +185,36 @@ class Model {
         // Effects
         let effects = [
             BackgroundGlucoseEffect,
-            
-            // Mock injection of 1U of insulin.
-            // 
-            fiaspInsulinEffect({
-                metabolism,
-                amount: 12,
-                injectionDate: startDate + 20 * MINUTE
-            }),
+        ]
+
+        const functionalEffects = [
+            compose(
+                functions.exercise(0.8),
+                functions.window({
+                    start: startDate + (20 * MINUTE),
+                    duration: 50*MINUTE
+                })
+            ),
 
             // Mock dumplings.
             // nom nom nom.
-            foodEffect({
-                amount: 80,
-                glycemicIndex: 63,
-                mealType: 'carbs',
-                ingestionDate: startDate
-            }),
+            compose(
+                functions.foodDigestionEffect(
+                    functions.foodDigestion('carbs', 80, 63 / 100)
+                ),
+                functions.beginsAfter({
+                    start: startDate,
+                })
+            ),
 
-        ]
-        const functionalEffects = [
-            exerciseEffect({
-                intensity: ExerciseIntensity.Cardio,
-                start: startDate,
-                duration: 50 * MINUTE
-            })
+            compose(
+                functions.insulinGlucoseEffect(
+                    functions.fiaspInsulinActive(12)
+                ),
+                functions.beginsAfter({
+                    start: startDate + 20 * MINUTE,
+                })
+            )
         ]
 
         // Current state
@@ -300,7 +228,6 @@ class Model {
             })
 
             const functionalSgv = functionalEffects.map(f => f(date)).reduce((prev, curr) => prev + curr, sgv)
-            console.log(functionalSgv)
 
             d.push({
                 sgv: functionalSgv,
