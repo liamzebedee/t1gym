7e4a39d855a7ca2272107fb679768908b9530de6
liamzebedee <liamzebedee@yahoo.com.au>
2020-06-16 21:22:48 +1000

    Add scripts for basal/bolus/correction experiments to come

---
+++ scripts/basal-test.js
@@ -0,0 +1,6 @@
+// How much to adjust basal down? 
+// Well, you can increase/decrease by 0.1U 
+// Or you can try to estimate what it needs to be.
+
+// BGs, start, end, basal rates.
+// Analyse from there.
---
+++ scripts/bolus-test.js
@@ -0,0 +1,6 @@
+// How much to adjust bolus down? 
+
+// BGs, start, end, basal rates.
+// Bolus event - amount, time, carbs.
+
+// Analyse from there.
