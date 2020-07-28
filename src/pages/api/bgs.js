76e39ae3f4307bcbfbc2b9dbbb60b1077446486e
liamzebedee <liamzebedee@yahoo.com.au>
2020-07-28 11:48:09 +1000

    api: fetch treatments/bgs directly from MongoDB
    This is quicker than using the REST API's from Nightscout.

--- src/pages/api/bgs.js
+++ src/pages/api/bgs.js
@@ -1,7 +1,79 @@
 import { Duration, DateTime } from 'luxon'
 
-export default async (req, res) => {
 
+
+import MongoClient from 'mongodb'
+const MONGO_DB_URL = process.env.MONGO_DB_URL
+if(!MONGO_DB_URL) {
+    throw new Error("MONGO_DB_URL is undefined. Cannot connect to Nightscout.")
+}
+
+async function fetchNightscoutData(range) {
+    const client = await MongoClient.connect(MONGO_DB_URL)
+    const db = client.db('heroku_hfwzth9r')
+
+    // Nightscout stores treatments with a timestamp field encoded as a UTC date string.
+    // Ordering with $gte/$lte in queries is based on string comparison,
+    // and so we have to normalise the dates we use to query into the UTC+0 timezone.
+    // This contrasts with entries, which have the numeric Unix timestamp as a `date` field.
+    // Sigh.
+    const treatmentTimeRange = range.map(v => {
+        return v.setZone('utc').toISO()
+    })
+
+    const treatments = await db.collection('treatments')
+        .find({
+            timestamp: {
+                $gte: treatmentTimeRange[0],
+                $lte: treatmentTimeRange[1],
+            }
+        })
+        .toArray()
+    
+    const sgvs = await db.collection('entries')
+        .find({
+            date: {
+                $gte: range[0].toMillis(),
+                $lte: range[1].toMillis(),
+            }
+        })
+        .toArray()
+    
+    return {
+        treatments,
+        data: sgvs,
+    }
+}
+
+// async function fetchTreatments(range) {
+//     const client = await MongoClient.connect(MONGO_DB_URL)
+//     const db = client.db('heroku_hfwzth9r')
+//     const treatments = await db.collection('treatments')
+//         .find({
+//             timestamp: {
+//                 $gte: range[0].toISO(),
+//                 $lte: range[1].toISO(),
+//             }
+//         })
+//         .toArray()
+//     return treatments
+// }
+
+// async function fetchSgvs(range) {
+//     const client = await MongoClient.connect(MONGO_DB_URL)
+//     const db = client.db('heroku_hfwzth9r')
+//     const treatments = await db.collection('entries')
+//         .find({
+//             date: {
+//                 $gte: range[0].toMillis(),
+//                 $lte: range[1].toMillis(),
+//             }
+//         })
+//         .toArray()
+//     return treatments
+// } 
+
+export default async (req, res) => {
     // Split data into day-by-day view.
     const DAYS_TO_RETRIEVE = 14
     let referenceDate = DateTime.local()
@@ -28,20 +100,22 @@ export default async (req, res) => {
     })
 
     ranges = ranges.reverse()
-
-    // ranges.map(range => {
-    //     console.log(range[0].toString(), range[1].toString())
-    // })
     
     const datums = ranges.map(async range => {
-        const url = `${process.env.NEXT_PUBLIC_NIGHTSCOUT_ENDPOINT_URL}/api/v1/entries/sgv.json?find[date][$gte]=${range[0].toMillis()}&find[date][$lte]=${range[1].toMillis()}&count=1000`
-        let data = await fetch(url)
-            .then(response => response.json())
-        console.log(url)
+        // const treatments = await fetchTreatments(range)
+        // const data = await fetchSgvs(range)
+        // console.log(data)
+        // const url = `${process.env.NEXT_PUBLIC_NIGHTSCOUT_ENDPOINT_URL}/api/v1/entries/sgv.json?find[date][$gte]=${range[0].toMillis()}&find[date][$lte]=${range[1].toMillis()}&count=1000`
+        // let data = await fetch(url)
+        //     .then(response => response.json())
+        // console.log(url)
+
         return {
             from: range[0].toString(),
             to: range[1].toString(),
-            data
+            ...await fetchNightscoutData(range)
+            // data,
+            // treatments
         }
     })
 
