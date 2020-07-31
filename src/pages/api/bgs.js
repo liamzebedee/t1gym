6fd6f0179b17e021139ea9b8e05f0b2a4602d298
liamzebedee <liamzebedee@yahoo.com.au>
2020-07-31 15:38:53 +1000

    api: remove NightScout REST API usage, fetch only through MongoDB Client

--- src/pages/api/bgs.js
+++ src/pages/api/bgs.js
@@ -1,22 +1,9 @@
 import { Duration, DateTime } from 'luxon'
-
-
-
 import MongoClient from 'mongodb'
-const MONGO_DB_URL = process.env.MONGO_DB_URL
-const MONGO_DB_NAME = process.env.MONGO_DB_NAME
-
-if(!MONGO_DB_URL) {
-    throw new Error("MONGO_DB_URL is undefined. Cannot connect to Nightscout.")
-}
-if(!MONGO_DB_NAME) {
-    throw new Error("MONGO_DB_NAME is undefined. Cannot connect to Nightscout.")
-}
+import { getMongoDatabase } from '../../api'
 
 async function fetchNightscoutData(range) {
-    const client = await MongoClient.connect(MONGO_DB_URL)
-    const db = client.db(MONGO_DB_NAME)
-
+    const db = await getMongoDatabase()
     // Nightscout stores treatments with a timestamp field encoded as a UTC date string.
     // Ordering with $gte/$lte in queries is based on string comparison,
     // and so we have to normalise the dates we use to query into the UTC+0 timezone.
@@ -42,6 +29,10 @@ async function fetchNightscoutData(range) {
                 $lte: range[1].toMillis(),
             }
         })
+        .project({
+            date: 1,
+            sgv: 1,
+        })
         .toArray()
     
     return {
@@ -49,34 +40,6 @@ async function fetchNightscoutData(range) {
         data: sgvs,
     }
 }
-
-// async function fetchTreatments(range) {
-//     const client = await MongoClient.connect(MONGO_DB_URL)
-//     const db = client.db('heroku_hfwzth9r')
-//     const treatments = await db.collection('treatments')
-//         .find({
-//             timestamp: {
-//                 $gte: range[0].toISO(),
-//                 $lte: range[1].toISO(),
-//             }
-//         })
-//         .toArray()
-//     return treatments
-// }
-
-// async function fetchSgvs(range) {
-//     const client = await MongoClient.connect(MONGO_DB_URL)
-//     const db = client.db('heroku_hfwzth9r')
-//     const treatments = await db.collection('entries')
-//         .find({
-//             date: {
-//                 $gte: range[0].toMillis(),
-//                 $lte: range[1].toMillis(),
-//             }
-//         })
-//         .toArray()
-//     return treatments
-// } 
 
 export default async (req, res) => {
     // Split data into day-by-day view.
@@ -99,7 +62,6 @@ export default async (req, res) => {
     // Now request data for all days.
     let ranges = []
     days.reduce((prev, curr) => {
-        // return [prev,curr]
         ranges.push([prev,curr])
         return curr
     })
@@ -107,20 +69,10 @@ export default async (req, res) => {
     ranges = ranges.reverse()
     
     const datums = ranges.map(async range => {
-        // const treatments = await fetchTreatments(range)
-        // const data = await fetchSgvs(range)
-        // console.log(data)
-        // const url = `${process.env.NEXT_PUBLIC_NIGHTSCOUT_ENDPOINT_URL}/api/v1/entries/sgv.json?find[date][$gte]=${range[0].toMillis()}&find[date][$lte]=${range[1].toMillis()}&count=1000`
-        // let data = await fetch(url)
-        //     .then(response => response.json())
-        // console.log(url)
-
         return {
             from: range[0].toString(),
             to: range[1].toString(),
             ...await fetchNightscoutData(range)
-            // data,
-            // treatments
         }
     })
 
--- src/pages/api/stats.js
+++ src/pages/api/stats.js
@@ -1,5 +1,5 @@
 import { Duration, DateTime } from 'luxon'
-
+import { fetchSgvs } from '../../api'
 
 export default async (req, res) => {
     const DAYS_TO_RETRIEVE = 7*5 // 5 weeks
@@ -13,21 +13,10 @@ export default async (req, res) => {
             second: 0,
             millisecond: 0
         })
-
+    
+    console.time('fetchSgvs')
     let range = [fromDate, toDate].map(x => DateTime.fromJSDate(new Date(x)))
-
-    const url = `${process.env.NEXT_PUBLIC_NIGHTSCOUT_ENDPOINT_URL}/api/v1/entries/sgv.json?find[date][$gte]=${range[0].toMillis()}&find[date][$lte]=${range[1].toMillis()}&count=10000000`
-    console.log(url)
-    let data = await fetch(url)
-        .then(response => response.json())
-
-    // // Run an algo on this data.
-    // let total = 0
-
-    // for(let datum of data) {
-    //     total += datum.sgv
-    // }
-    // console.log(total)
-
+    const data = await fetchSgvs(range)
+    console.timeEnd('fetchSgvs')
     res.status(200).json(data)
 }
