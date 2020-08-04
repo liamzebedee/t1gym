434f590611f14f42adcef0f0387103e2fff2ee4a
liamzebedee <liamzebedee@yahoo.com.au>
2020-08-04 20:20:53 +1000

    fix: refresh Firebase token session cookie

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -27,8 +27,17 @@ export default function MyApp({ Component, pageProps }) {
     firebase.initializeApp(firebaseConfig)
     firebase.analytics()
     
-    firebase.auth().onAuthStateChanged(function(user) {
+    const auth = firebase.auth()
+    auth.onAuthStateChanged(async function(user) {
       if (user) {
+        // For later calls to the backend, we use the `token` as a session cookie.
+        // It will be used to identify the user using the Firebase API.
+        // Since Firebase auth tokens expire after a certain amount of time,
+        // we refresh the token upon load.
+        const token = await user.getIdToken(true)
+        console.debug(`Setting Firebase token to ${token}`)
+        document.cookie = `token=${token}; path=/`
+
         setLoaded(true)
       }
     })
