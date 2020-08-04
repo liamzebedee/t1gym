294578fdc437d97eceac8d101eb9bf225feb7946
liamzebedee <liamzebedee@yahoo.com.au>
2020-08-04 20:06:47 +1000

    fix: block on Firebase auth before rendering App

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -7,29 +7,35 @@ import "firebase/analytics";
 import "firebase/auth";
 import "firebase/firestore";
 
-function initialiseFirebase() {
-  const firebaseConfig = {
-    apiKey: "AIzaSyCb_ngSJOr7w-SIVWmt_wTMnQVFFE9b_VM",
-    authDomain: "diasim.firebaseapp.com",
-    databaseURL: "https://diasim.firebaseio.com",
-    projectId: "diasim",
-    storageBucket: "diasim.appspot.com",
-    messagingSenderId: "995306789526",
-    appId: "1:995306789526:web:91f4707e70324557a4c0b7",
-    measurementId: "G-8WLEW1EBLM"
-  }
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig)
-  firebase.analytics();
-}
-
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   const [loaded, setLoaded] = useState(false)
+
+  function initialiseFirebase() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyCb_ngSJOr7w-SIVWmt_wTMnQVFFE9b_VM",
+      authDomain: "diasim.firebaseapp.com",
+      databaseURL: "https://diasim.firebaseio.com",
+      projectId: "diasim",
+      storageBucket: "diasim.appspot.com",
+      messagingSenderId: "995306789526",
+      appId: "1:995306789526:web:91f4707e70324557a4c0b7",
+      measurementId: "G-8WLEW1EBLM"
+    }
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig)
+    firebase.analytics()
+    
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        setLoaded(true)
+      }
+    })
+  }
+
   useEffect(() => {
     initialiseFirebase()
-    setLoaded(true)
   }, [])
 
   if(!loaded) return null
