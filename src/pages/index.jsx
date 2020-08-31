008b516392afd96bd4354b0378afb845c30c3bd3
liamzebedee <liamzebedee@yahoo.com.au>
2020-08-31 23:19:26 +1000

    ux: add <title>

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,2 +1,10 @@
 import { App } from '../components/App'
-export default App
+import Head from 'next/head'
+
+export default () => <>
+    <Head>
+        <title>T1 Gym</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <App/>
+</>
