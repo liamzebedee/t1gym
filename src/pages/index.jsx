78717748f50643d899d196f5d767919eecfe42a5
liamzebedee <liamzebedee@yahoo.com.au>
2020-08-31 23:44:12 +1000

    ux: favicon

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,6 +5,7 @@ export default () => <>
     <Head>
         <title>T1 Gym</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/favicon.png"/>
     </Head>
     <App/>
 </>
