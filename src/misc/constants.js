8e4f19e14f4e24671e4d5934728e46dcdd3653a6
liamzebedee <liamzebedee@yahoo.com.au>
2020-09-07 17:34:27 +1000

    fix: rejig profile bgTargetTop

--- src/misc/constants.js
+++ src/misc/constants.js
@@ -3,7 +3,7 @@ const SCALE = 18.
 export const PROFILE = {
     targetRange: {
         bgHigh: 14.4 * SCALE,
-        bgTargetTop: 8.5 * SCALE,
+        bgTargetTop: 9.5 * SCALE,
         bgTargetBottom: 4.4 * SCALE,
         bgLow: 3.05 * SCALE
     }
