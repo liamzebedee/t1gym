fe8e3712eba26857bd83a91fadf10fb0e1bd6af0
liamzebedee <liamzebedee@yahoo.com.au>
2020-09-11 15:32:59 +1000

    storybook: add storybook fixtures for Chart

--- stories/fixtures/index.js
+++ stories/fixtures/index.js
@@ -1,4 +1,10 @@
 const basalEntries = require("./basal-entries.json")
+const bgs = require('./bgs.json')
+const treatments = require('./treatments.json')
+
+
 export {
-    basalEntries
+    basalEntries,
+    bgs,
+    treatments
 }
